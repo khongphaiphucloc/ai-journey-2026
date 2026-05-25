4493b362f459185a6340fbc6232df812944687c5
Loc <nguyenphucloc229@gmail.com>
2026-05-25 11:29:45 +0700

    Add first Python experiment

---
+++ experiments/hello-python/main.py
@@ -0,0 +1,7 @@
+﻿name = input("Tên của bạn là gì? ")
+goal = input("Bạn muốn học gì? ")
+
+print()
+print("Xin chào", name)
+print("Mục tiêu học của bạn là:", goal)
+print("Bước đầu tiên: học Python cơ bản và dùng GitHub mỗi ngày.")
